fe434aef4a73d295138318163a5cecf62dac36da
Lachie <92796015+lachlanpywell@users.noreply.github.com>
2022-11-20 14:50:29 +1100

    Program to tests force sensing resistors, and roll hall effect sensors

---
+++ SensorTests/SensorTests.ino
@@ -0,0 +1,71 @@
+/*
+SENSOR TESTING
+*/
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+  // -------------
+  // FSRs
+  // -------------
+  int FSR_0 = analogRead(A0);
+  int FSR_1 = analogRead(A1);
+  int FSR_2 = analogRead(A2);
+  int FSR_3 = analogRead(A3);
+  int FSR_4 = analogRead(A4);
+  int FSR_5 = analogRead(A5);
+
+  Serial.print("min:");
+  Serial.print(0);
+  Serial.print(",");
+  Serial.print("max:");
+  Serial.print(1023);
+  Serial.print(",");
+  Serial.print("FSR_0:");
+  Serial.print(FSR_0);
+  Serial.print(",");
+  Serial.print("FSR_1:");
+  Serial.print(FSR_1);
+  Serial.print(",");
+  Serial.print("FSR_2:");
+  Serial.print(FSR_2);
+  Serial.print(",");
+  Serial.print("FSR_3:");
+  Serial.print(FSR_3);
+  Serial.print(",");
+  Serial.print("FSR_4:");
+  Serial.print(FSR_4);
+  Serial.print(",");
+  Serial.print("FSR_5:");
+  Serial.println(FSR_5);
+
+  // -------------
+  // Roll/SC Hall Effect
+  // -------------
+  int HE_8 = analogRead(A8);
+  int HE_9 = analogRead(A9);
+  int HE_10 = analogRead(A10);
+
+  Serial.print("min:");
+  Serial.print(0);
+  Serial.print(",");
+  Serial.print("max:");
+  Serial.print(1023);
+  Serial.print(",");
+  Serial.print("HE_8:");
+  Serial.print(HE_8);
+  Serial.print(",");
+  Serial.print("HE_9:");
+  Serial.print(HE_9);
+  Serial.print(",");
+  Serial.print("HE_10:");
+  Serial.println(HE_10);
+
+  delay(1);  // delay in between reads for stability
+}
