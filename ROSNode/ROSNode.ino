a6b2a189166a043907c9a69277c75018ba27dc24
Lachie <92796015+lachlanpywell@users.noreply.github.com>
2022-12-11 19:28:19 +1100

    Add pitch and yaw speed mapping based on encoder val
    Untested

--- ROSNode/ROSNode.ino
+++ ROSNode/ROSNode.ino
@@ -12,20 +12,22 @@
 // 4: Spare (always zero)
 // Shared Control state is computed but not currently written to the topic
 
-bool ROS_MODE = true;  // false = Serial, true = ROS
+bool ROS_MODE = true;  // false = Serial (used for debugging), true = ROS
+
+// ROS Publisher
 std_msgs::Float64MultiArray cmdMsgArrayFloat;
 ros::Publisher FootInterface("interface_cmd", &cmdMsgArrayFloat);
 ros::NodeHandle nh;
 
 // Create Global Objects
-ACE128 myACE(36, 38, 40, 42, 28, 30, 32, 34, (uint8_t*)encoderMap_12345678);
+ACE128 myACE(36, 38, 40, 42, 28, 30, 32, 34, (uint8_t*)encoderMap_12345678); // encoder
 
 // CONSTANTS
 // Hall Effect Sensors
 const int ROLL_THRESHOLD_SLOW = 350;
 const int ROLL_THRESHOLD_FAST = 480;
-const int SHARED_CONTROL_ACTIVATE = 300;
-const float ROTATION_SPEED_INCREMENT = 0.00002;
+const int SHARED_CONTROL_ACTIVATE = 300; // threshold read from hall effect sensor to toggle shared control mode on/off
+const float ROTATION_SPEED_INCREMENT = 0.00002; // when transitioning from slow to fast the speed increments by this amount each loop until the faster speed is reached
 const float MIN_SPEED_ROTATION = 0.02;
 const float MAX_SPEED_ROTATION = 0.04;
 
@@ -39,10 +41,35 @@ const float MAX_SPEED_INOUT = 0.007;
 const float INOUT_SPEED_INCREMENT = 0.00001;
 
 // Encoders
+// LR = Left/Right encoder
+// FB = Forward/Back encoder
 const int ENCODER_LR_FULL_SCALE = 1023;
 const int ENCODER_FB_FULL_SCALE = 128;
 const int ENCODER_LR_RANGE = 800; // i.e. 0-90 is the max revolution possible rotation of the gear
 const int ENCODER_FB_RANGE = 90;
+
+const int ENCODER_LR_HOME = 500; // TO BE TESTED
+const int ENCODER_LR_WINDOW = 25;// TO BE TESTED
+const int ENCODER_FB_HOME = 60;// TO BE TESTED
+const int ENCODER_FB_WINDOW = 4;// TO BE TESTED
+
+const int ENCODER_LR_UPPER_SLOW = 700;
+const int ENCODER_LR_UPPER_FAST = 900;
+const int ENCODER_LR_LOWER_SLOW = 300;
+const int ENCODER_LR_LOWER_FAST = 100;
+
+const int ENCODER_FB_UPPER_SLOW = 110;
+const int ENCODER_FB_UPPER_FAST = 80;
+const int ENCODER_FB_LOWER_SLOW = 40;
+const int ENCODER_FB_LOWER_FAST = 10;
+
+const int MAX_SPEED_PITCH = 0.05;
+const int MIN_SPEED_PITCH = 0.02;
+const int PITCH_SPEED_INCREMENT = 0.00002;
+
+const int MAX_SPEED_YAW = 0.06;
+const int MIN_SPEED_YAW = 0.02;
+const int YAW_SPEED_INCREMENT = 0.00002;
 
 // Array indices
 const int ROS_ROLL = 0;
@@ -203,7 +230,7 @@ void loop() {
   temp3 = analogRead(FSR_5_PIN);
   withdrawalPressure = (temp1 + temp2 + temp3) / 3; // average pressure sensor readings
 
-  pressureStates(insertionPressure, currStateIns);
+  pressureStates(insertionPressure, currStateIns); // pressure sensors either ON/OFF based on current reading (acting like a switch)
   pressureStates(withdrawalPressure, currStateWith);
 
   // Detect if the foot is lifted off the pedal
@@ -222,9 +249,6 @@ void loop() {
   withdrawalSpeed = doubleTapSpeed(countTapWith, withdrawalPressure, insertionPressure, -1);
 
   cmdArrayFloat[ROS_INOUT] = insertionSpeed > abs(withdrawalSpeed) ? insertionSpeed : withdrawalSpeed;
-  //  Serial.print(countTapWith);
-  //  Serial.print(" , ");
-//  Serial.println(cmdArrayFloat[ROS_INOUT], 4);
 
   prevStateIns = currStateIns;
   prevStateWith = currStateWith;
@@ -243,6 +267,29 @@ void loop() {
     limitSwitch2State = digitalRead(LIMIT_SWITCH_2_PIN);
     encoderValFB = myACE.rawPos();
     encoderValMappedFB = encoderMapping(encoderValFB, limitSwitch1State, limitSwitch2State, encoderLimitSwitchFB , ENCODER_FB_FULL_SCALE, ENCODER_FB_RANGE);
+
+    if (encoderValMappedFB < ENCODER_FB_LOWER_FAST) {
+      // ramp to max speed
+      if (cmdArrayFloat[ROS_PITCH] < -MAX_SPEED_PITCH) {
+        cmdArrayFloat[ROS_PITCH] -= PITCH_SPEED_INCREMENT;
+      }
+    } else if (encoderValMappedFB < ENCODER_FB_LOWER_SLOW || (cmdArrayFloat[ROS_PITCH] = -MIN_SPEED_PITCH && encoderValMappedFB < (ENCODER_FB_HOME - ENCODER_FB_WINDOW))) {
+      // slow speed
+      cmdArrayFloat[ROS_PITCH] = -MIN_SPEED_PITCH;
+    }
+    else if (encoderValMappedFB > ENCODER_FB_UPPER_FAST) {
+      if (cmdArrayFloat[ROS_PITCH] < MAX_SPEED_PITCH) {
+        cmdArrayFloat[ROS_PITCH] += PITCH_SPEED_INCREMENT;
+      }
+    }
+    else if (encoderValMappedFB > ENCODER_FB_UPPER_SLOW || (cmdArrayFloat[ROS_PITCH] = MIN_SPEED_PITCH && encoderValMappedFB > (ENCODER_FB_HOME + ENCODER_FB_WINDOW))) {
+      // slow speed
+      cmdArrayFloat[ROS_PITCH] = MIN_SPEED_PITCH;
+    }
+    else {
+      cmdArrayFloat[ROS_PITCH] = 0.0;
+    }
+
   }
 
 
@@ -278,14 +325,35 @@ void loop() {
     digitalWrite(CLOCK_PIN, HIGH);
 
     encoderValMappedLR = encoderMapping(encoderValLR, limitSwitch3State, limitSwitch4State, encoderLimitSwitchLR, ENCODER_LR_FULL_SCALE, ENCODER_LR_RANGE);
+    if (encoderValMappedLR < ENCODER_LR_LOWER_FAST) {
+      // ramp to max speed
+      if (cmdArrayFloat[ROS_YAW] < -MAX_SPEED_YAW) {
+        cmdArrayFloat[ROS_YAW] -= YAW_SPEED_INCREMENT;
+      }
+    } else if (encoderValMappedLR < ENCODER_LR_LOWER_SLOW || (cmdArrayFloat[ROS_YAW] = -MIN_SPEED_YAW && encoderValMappedLR < (ENCODER_LR_HOME - ENCODER_LR_WINDOW))) {
+      // slow speed
+      cmdArrayFloat[ROS_YAW] = -MIN_SPEED_YAW;
+    }
+    else if (encoderValMappedLR > ENCODER_LR_UPPER_FAST) {
+      if (cmdArrayFloat[ROS_YAW] < MAX_SPEED_YAW) {
+        cmdArrayFloat[ROS_YAW] += YAW_SPEED_INCREMENT;
+      }
+    }
+    else if (encoderValMappedLR > ENCODER_LR_UPPER_SLOW || (cmdArrayFloat[ROS_YAW] = MIN_SPEED_YAW && encoderValMappedLR > (ENCODER_LR_HOME + ENCODER_LR_WINDOW))) {
+      // slow speed
+      cmdArrayFloat[ROS_YAW] = MIN_SPEED_YAW;
+    }
+    else {
+      cmdArrayFloat[ROS_YAW] = 0.0;
+    }
   }
 
   itoa(encoderValMappedLR, tempROSLogging, 10);
   nh.loginfo(tempROSLogging);
-  
+
   itoa(encoderValMappedFB, tempROSLogging, 10);
   nh.loginfo(tempROSLogging);
-  
+
   // ------------------- //
   // SHARED CONTROL
   // ------------------- //
@@ -322,9 +390,11 @@ void loop() {
   delay(1);  // for analog read stability
 }
 
+
 // ------------------- //
-// INSERTION/WITHDRAWAL
+// FUNCTIONS
 // ------------------- //
+// Determine if the force sensing resistor is in the ON or OFF state based on current pressure reading and the pressure threshold/setpoint
 void pressureStates(int pressureVal, bool &currState) {
   // Check values against threshold
   if (pressureVal < FSR_SLOW_THRESHOLD) {
@@ -334,6 +404,7 @@ void pressureStates(int pressureVal, bool &currState) {
   }
 }
 
+// Count the number of state changes occuring in a given time period (i.e. count the number of taps performed on each group of sensors)
 void doubleTapCount(bool currStateA, bool currStateB, bool prevStateA, bool prevStateB, int &countTap, long &startTime, long &timeLast, long &timeA) {
 
   // Record State Change Times (when sensor detects foot lift or tap on insertion/withdrawal sensors)
@@ -361,10 +432,11 @@ void doubleTapCount(bool currStateA, bool currStateB, bool prevStateA, bool prev
   }
 }
 
-// Calculate if the insertion/withdrawal command is the low, high or off speed.
+// Calculate if the insertion/withdrawal command is the low, high or off speed based on the current pressure reading
+// and number of taps performed
 float doubleTapSpeed(int &countTap, int pressureA, int pressureB, int dir) {
   float output = 0.0;
-  if (countTap == 4) {
+  if (countTap == 4) { // i.e. OFF->ON->OFF->ON = 4
     if (pressureA > FSR_SLOW_THRESHOLD) {
       if (pressureB < FSR_FAST_THRESHOLD) {
         if (cmdArrayFloat[ROS_INOUT]*dir < MAX_SPEED_INOUT) {
@@ -377,16 +449,14 @@ float doubleTapSpeed(int &countTap, int pressureA, int pressureB, int dir) {
         output = MIN_SPEED_INOUT * dir;
       }
     }
-    //  if (cmdArrayFloat[ROS_ROLL] > -MAX_SPEED_ROTATION) {
-    //      cmdArrayFloat[ROS_ROLL] -= ROTATION_SPEED_INCREMENT;
-    //    }
-
   }
 
   return output;
 }
 
-// Map the encoder raw/absolute value to 0 - Full Scale (i.e. all the way to one end will read zero)
+// Map the encoder raw/absolute value to 0 - Configured Full Max (not necessarilly full scale) (i.e. all the way to one end will read zero, other end read configured max)
+// This ensures if the encoders are removed from the assembly when placed in have rotated to a new location (absolute encoders) the setpoints used for the home position
+// and slow/fast spots do not need to be updated, rather they can be 'recalibrated' in code using the limit switches
 int encoderMapping(int rawVal, bool limitSwitch1, bool limitSwitch2, int limitSwitchZero, int ENCODER_FULL_SCALE, int FULL_RANGE) {
   int output = 0;
 
